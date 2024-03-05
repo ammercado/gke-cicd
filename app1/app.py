046fb76413f8bd9cb67a45e7ab58601490cae53d
ammercado <a.mauriciomercado.it@gmail.com>
2024-03-05 16:22:39 -0300

    update deploy 5

--- app1/app.py
+++ app1/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World App 1!'
+     return render_template("home.html")
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
